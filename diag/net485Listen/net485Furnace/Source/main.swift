eda467b16332e47421e2c198f5fce26ec6b97b6d
Kevin Peck <kevindpeck@gmail.com>
2020-05-02 16:09:51 -0400

    placeholder for furnace client

---
+++ diag/net485Listen/net485Furnace/Source/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  net485Furnace
+//
+//  Created by Kevin Peck on 2020-05-02.
+//  Copyright © 2020 Kevin Peck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
